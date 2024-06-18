e21010e555bcc1aa6c7034bbea53e5a8187cc42c
Jaikrishnavr01 <jaikrishnavr@outlook.com>
2024-06-18 19:22:38 +0530

    gemini ai save prompt in sidebar

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -20,21 +20,35 @@ const ContextProvider = (props) => {
 
     }
 
+    const newChat = () => {
+        setLoading(false)
+        setShowResult(false)
+    }
+
     const onSent = async (prompt) => {
 
         setResultData("")
         setLoading(true)
         setShowResult(true)
-        setRecentPrompt(input)
-        const response = await runChat(input)
+        let response;
+        if(prompt !== undefined) {
+             response = await runChat(prompt);
+             setRecentPrompt(prompt)
+        }
+        else {
+            setPrevPrompts(prev => [...prev,input])
+            setRecentPrompt(input)
+            response = await runChat(input)
+        }
+
         let responseArray = response.split("**");
-        let newResponse;
+        let newResponse="";
         for(let i = 0; i < responseArray.length; i++){
             if(i === 0 || i%2 !== 1){
                 newResponse += responseArray[i];
             }
             else{
-                newResponse += "<b>"+ responseArray[i]+"</b>";
+                newResponse += "<b>"+ responseArray[i]+"</b> ";
             }
         }
         let newResponse2 = newResponse.split("*").join("</br>")
@@ -58,7 +72,8 @@ const ContextProvider = (props) => {
         loading,
         resultData,
         input,
-        setInput
+        setInput,
+        newChat
     }
 
     return (
