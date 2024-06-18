987f1c518ded4e566f4cc84310dbd342128c4eb9
Jaikrishnavr01 <jaikrishnavr@outlook.com>
2024-06-18 17:09:41 +0530

    gemini ai typing animation

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -10,19 +10,41 @@ const ContextProvider = (props) => {
     const [recentPrompt, setRecentPrompt] = useState("");
     const [prevPrompts, setPrevPrompts] = useState([]);
     const [showResult, setShowResult] = useState(false);
-    const [loading,setLoading] = useState(false)
+    const [loading, setLoading] = useState(false)
     const [resultData, setResultData] = useState("")
 
-    const onSent  = async (prompt) => {
+    const delayPara = (index, nextWord) => {
+        setTimeout(function () {
+            setResultData(prev =>  prev+nextWord);
+        },75*index)
+
+    }
+
+    const onSent = async (prompt) => {
 
         setResultData("")
         setLoading(true)
         setShowResult(true)
-    setRecentPrompt(input)
-      const response = await runChat(input)
-      setResultData(response)
-      setLoading(false)
-      setInput("")
+        setRecentPrompt(input)
+        const response = await runChat(input)
+        let responseArray = response.split("**");
+        let newResponse;
+        for(let i = 0; i < responseArray.length; i++){
+            if(i === 0 || i%2 !== 1){
+                newResponse += responseArray[i];
+            }
+            else{
+                newResponse += "<b>"+ responseArray[i]+"</b>";
+            }
+        }
+        let newResponse2 = newResponse.split("*").join("</br>")
+        let newResponseArray = newResponse2.split(" ");
+        for(let i=0; i<newResponseArray.length; i++){
+            const nextWord = newResponseArray[i];
+            delayPara(i, nextWord+ " ")
+        }
+        setLoading(false)
+        setInput("")
 
     }
 
@@ -45,5 +67,5 @@ const ContextProvider = (props) => {
         </Context.Provider>
     )
 }
- 
+
 export default ContextProvider;
