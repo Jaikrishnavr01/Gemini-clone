f6ec91df92891e2573160557b0eec42a54aedb4c
Jaikrishnavr01 <jaikrishnavr@outlook.com>
2024-06-18 15:33:04 +0530

    gemini ai loader

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,7 +5,7 @@ import { Context } from '../../context/Context'
 
 const Main = () => {
 
-    const { onSent,recentPrompt,showResult,loading, resultData,input,setInput } = useContext(Context)
+    const { onSent, recentPrompt, showResult, loading, resultData, input, setInput } = useContext(Context)
 
     return (
         <div className='main'>
@@ -15,42 +15,48 @@ const Main = () => {
             </div>
             <div className='main-container'>
                 {!showResult ? <>
-                <div className="greet">
-                    <p><span>Hello, dev.</span></p>
-                    <p>How Can I Help You Today?</p>
-                </div>
-                <div className="cards">
-                    <div className="card">
-                        <p>Suggest beautiful place to see on upcoming road trip</p>
-                        <img src={assets.compass_icon} alt="" />
+                    <div className="greet">
+                        <p><span>Hello, dev.</span></p>
+                        <p>How Can I Help You Today?</p>
                     </div>
-                    <div className="card">
-                        <p>Brifefly Summarize this concept: urban planning</p>
-                        <img src={assets.bulb_icon} alt="" />
+                    <div className="cards">
+                        <div className="card">
+                            <p>Suggest beautiful place to see on upcoming road trip</p>
+                            <img src={assets.compass_icon} alt="" />
+                        </div>
+                        <div className="card">
+                            <p>Brifefly Summarize this concept: urban planning</p>
+                            <img src={assets.bulb_icon} alt="" />
+                        </div>
+                        <div className="card">
+                            <p>Brainstrom team bonding activity for our work retreat</p>
+                            <img src={assets.message_icon} alt="" />
+                        </div>
+                        <div className="card">
+                            <p>Improve the readability of the folling code</p>
+                            <img src={assets.code_icon} alt="" />
+                        </div>
                     </div>
-                    <div className="card">
-                        <p>Brainstrom team bonding activity for our work retreat</p>
-                        <img src={assets.message_icon} alt="" />
-                    </div>
-                    <div className="card">
-                        <p>Improve the readability of the folling code</p>
-                        <img src={assets.code_icon} alt="" />
-                    </div>
-                </div>
-                
-                </>: <div className='result'> 
+
+                </> : <div className='result'>
                     <div className='result-title'>
                         <img src={assets.user_icon} alt="" />
                         <p>{recentPrompt}</p>
                     </div>
                     <div className="result-data">
                         <img src={assets.gemini_icon} alt="" />
-                        <p dangerouslySetInnerHTML={{__html:resultData}}></p>
+                        {loading ? <div className='loader'>
+                            <hr />
+                            <hr />
+                            <hr />
+                        </div>
+                            : <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+                        }
                     </div>
-                </div> }
+                </div>}
                 <div className="main-bottom">
                     <div className="search-box">
-                        <input onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder='Enter a prompt here'/>
+                        <input onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder='Enter a prompt here' />
                         <div>
                             <img src={assets.gallery_icon} alt="" />
                             <img src={assets.mic_icon} alt="" />
@@ -58,7 +64,7 @@ const Main = () => {
                         </div>
                     </div>
                     <p className='bottom-info'>
-                    Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps
+                        Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps
                     </p>
                 </div>
             </div>
